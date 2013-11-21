32a07ded949bc96cbadfc23349ccc8300172390c
Aurélien Noce <aurelien.noce@imagine-app.fr>
2013-11-21 16:31:00 +0100

    Improve sprinkle script

--- packages.rb
+++ packages.rb
@@ -1,3 +1,13 @@
+require 'set'
+
+$users ||= Set.new
+
+# utilities
+package :apt_update do
+  description 'Update apt cache'
+  runner 'apt-get update'
+end
+
 package :python_software_properties do
   apt 'python-software-properties'
   verify do
@@ -6,7 +16,7 @@ package :python_software_properties do
 end
 
 package :ppa do
-  description 'support for ppa repositories'
+  description 'support for ppa repositories with apt-add-repository'
   requires :python_software_properties
   verify do
     has_executable 'apt-add-repository'
@@ -14,11 +24,100 @@ package :ppa do
 end
 
 package :build_essential do
-  apt 'build-essential' do
-    pre :install, 'apt-get update'
-  end
+  apt 'build-essential'
   verify do
     has_apt 'build-essential'
+  end
+end
+
+package :ssh do
+  description 'OpsnSSH client and daemon'
+  apt %w{openssh-client openssh-server}
+  verify do
+    %w{sshd ssh}.each { |e| has_executable e }
+    has_process 'sshd'
+  end
+end
+
+package :update_locales, sudo: true do
+  apt %w{en fr}.collect { |l| "language-pack-#{l}" } do
+    post :install, 'locale-gen'
+    post :install, 'update-locale LANGUAGE=en_US.UTF8 LC_MESSAGES=POSIX'
+    post :install, 'dpkg-reconfigure locales'
+  end
+end
+
+# create users
+if $users.any?
+  $users.each do |user|
+    package "create_#{user}".to_sym do
+      #requires "create_#{user}_group"
+      add_user user, in_group: user, flags: "--disabled-password"
+      verify do
+        has_user user, in_group: user
+      end
+     end
+  end
+end
+
+package :create_users do
+  $users.each do |user|
+    requires "create_#{user}"
+  end
+end
+
+# scm
+package :git, provides: :scm do
+  apt 'git-core'
+  verify do
+    has_apt 'git'
+    has_executable 'git'
+  end
+end
+
+# firewall
+package :ufw_add_profile do
+  requires :ufw
+  runner "ufw allow \"#{opts[:profile]}\""
+  verify do
+    runs_without_error "ufw status | grep \"#{opts[:profile]}\""
+  end
+end
+
+package :ufw, provides: :firewall do
+  apt 'ufw'
+  runner 'ufw default deny'
+  runner 'ufw allow OpenSSH'
+  runner  'ufw --force enable'
+
+  verify do
+    has_executable 'ufw'
+    runs_without_error 'ufw status | grep "Status: active"'
+    runs_without_error 'ufw status | grep "OpenSSH"'
+  end
+end
+
+# security packages
+package :fail2ban do
+  requires :ssh
+  apt 'fail2ban'
+  verify do
+    has_apt 'fail2ban'
+  end
+end
+
+package :ssh_hardening do
+  key = File.read(File.expand_path('~/.ssh/id_rsa.pub')).chomp
+
+  push_text key, '~/.ssh/authorized_keys'
+  replace_text 'PasswordAuthentication yes', 'PasswordAuthentication no', '/etc/ssh/sshd_config'
+  replace_text 'X11Forwarding yes', 'X11Forwarding no', '/etc/ssh/sshd_config'
+  replace_text 'UsePAM yes', 'UsePAM no', '/etc/ssh/sshd_config'
+  verify do
+    file_contains '~/.ssh/authorized_keys', key
+    file_contains '/etc/ssh/sshd_config', 'PasswordAuthentication no'
+    file_contains '/etc/ssh/sshd_config', 'X11Forwarding no'
+    file_contains '/etc/ssh/sshd_config', 'UsePAM no'
   end
 end
 
@@ -29,11 +128,12 @@ package :nginx, provides: :webserver do
   apt 'nginx-full' do
     pre :install, "apt-add-repository ppa:nginx/stable"
     pre :install, "apt-get update"
+    post :install, 'update-rc.d nginx defaults'
   end
   verify do
     has_apt 'nginx-full'
     has_user 'www-data'
-    has_process 'nginx'
+    has_executable 'nginx'
   end
 end
 
@@ -72,7 +172,19 @@ end
 
 # cache
 package :redis, provides: :cache do
-  requires :build_essentials
+  description 'install Redis using apt'
+  apt 'redis-server'
+  verify do
+    has_apt 'redis-server'
+    has_executable 'redis-server'
+  end
+end
+
+package :redis_source, provides: :cache do
+  description 'install Redis from source'
+  requires :build_essential
+
+
   source "http://download.redis.io/redis-stable.tar.gz" do
     pre :install, "killall -9 redis-server || true"
     custom_install 'make install'
@@ -83,11 +195,48 @@ package :redis, provides: :cache do
   end
 end
 
-policy :base, roles: :base do
-  requires :build_essential
+package :memcached, provides: :cache do
+  description 'install Memcache using apt'
+  apt 'memcached'
+  verify do
+    has_apt 'memcached'
+    has_executable 'memcached'
+  end
 end
 
-policy :rails, roles: :app do
+# meta packages
+package :base_tools do
+  description 'install base tools for box administration'
+  %w{scm ssh}.each { |d| requires d }
+  apt %w{wget curl vim-nox}
+  verify do
+    %w{wget curl vim}.each { |e| has_executable e }
+  end
+end
+
+package :base_security do
+  requires :fail2ban
+  requires :ssh_hardening
+end
+
+package :base do
+  description 'Stuff I install on all servers'
+  requires :apt_update
+  requires :update_locales
+  requires :build_essential
+  requires :base_tools
+  requires :base_security
+  requires :create_users if $users.any?
+end
+
+# policies
+
+policy :webserver, roles: :webserver  do
+  requires :base
   requires :webserver
   requires :database
+  requires :cache
+  requires :firewall
+  requires :ufw_add_profile, profile: 'Nginx Full'
 end
+
--- vagrant.rb
+++ vagrant.rb
@@ -1,13 +1,20 @@
+$users = ['toto']
 require File.join(File.dirname(__FILE__),'packages.rb')
 
 deployment do
   delivery :capistrano do
-    role :base, "127.0.0.1"
-    role :app, "127.0.0.1"
+    role :webserver, "127.0.0.1"
+
     set :user, "vagrant"
     set :run_method, :sudo
     ssh_options[:keys] = ["#{ENV['HOME']}/.vagrant.d/insecure_private_key"]
     ssh_options[:port] = 2222
+  end
+
+  source do
+    prefix   '/usr/local'           # where all source packages will be configured to install
+    archives '/usr/local/sources'   # where all source packages will be downloaded to
+    builds   '/usr/local/build'     # where all source packages will be built
   end
 end
 
